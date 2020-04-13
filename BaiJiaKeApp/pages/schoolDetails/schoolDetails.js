f8ec54656125ad3fb3055ad0a9379ab0471e0b4a
张晨光 <chenguang.zhang@leyoutech.com>
2020-04-13 19:04:12 +0800

    新增商家详情页

--- BaiJiaKeApp/pages/schoolDetails/schoolDetails.js
+++ BaiJiaKeApp/pages/schoolDetails/schoolDetails.js
@@ -15,39 +15,29 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  openMap: function () {
+    wx.getLocation({
+      type: 'gcj02', //返回可以用于wx.openLocation的经纬度
+      success(res) {
+        const latitude = res.latitude
+        const longitude = res.longitude
+        wx.openLocation({
+          latitude,
+          longitude,
+          scale: 18,
+          name: "阿蒂朵芭舞蹈工作室"
+        })
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  toSchoolDetails: function () {
+    wx.navigateTo({
+      url: '../schoolDetails/schoolDetails',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
   },
 
   /**
