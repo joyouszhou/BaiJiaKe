cafbf1d8cdb10a365e43eab260f97ed1da3ac739
initial_yang <initial_yang@163.com>
2020-05-04 16:33:24 +0800

    店铺ui优化

--- BaiJiaKeApp/pages/schoolDetails/schoolDetails.js
+++ BaiJiaKeApp/pages/schoolDetails/schoolDetails.js
@@ -14,6 +14,9 @@ Page({
     isShow: true,
     courseList:[],
     courseList1: [],
+    isShouCang: false,
+    offset: 0,
+    offset1: 0,
     baseUrl: app.globalData.baseUrl
   },
 
@@ -26,10 +29,21 @@ Page({
     
     eventChannel.on('acceptDataFromOpenerPage', function (data) {
       data.data.tagList = data.data.shopinfo.tags !== '' ? data.data.shopinfo.tags.split(',') : null
+      var str = data.data.shopinfo.institution;
+      var re = /{(.*?)}/g;
+      var array = [];
+      var temp = [];
+      while (temp = re.exec(str)) {
+        array.push(temp[0].substring(1,temp[0].length - 1))
+        array.push(temp[0].substring(1,temp[0].length - 1))
+        array.push(temp[0].substring(1,temp[0].length - 1))
+      }
+      data.data.institutionList = array
       that.setData({
         shopData:data.data,
         imgLength: data.data.imageurl.length
       })
+      
       console.log(data.data)
     })
     wx.request({
@@ -41,11 +55,22 @@ Page({
         // console.log(res.data.data.shops)
       }
     })
+    wx.showShareMenu({
+      withShareTicket: true
+    })
     wx.request({
       url: app.globalData.baseUrl +'/v1/course?limit=3&offset=0&sortby={"created_at": "desc"}&course_status=1',
       success : function (res) {
         that.setData({
           courseList: res.data.data.course
+        })
+      }
+    })
+    wx.request({
+      url: app.globalData.baseUrl +'/v1/course?limit=3&offset=0&sortby={"created_at": "desc"}&course_status=2',
+      success : function (res) {
+        that.setData({
+          courseList1: res.data.data.course
         })
       }
     })
@@ -151,4 +176,105 @@ Page({
       complete: function (res) { },
     })
   },
+  fenXiang: function () {
+    wx.showShareMenu({
+      withShareTicket: true
+    })
+  },
+  shouCang: function(){
+    let that = this;
+    let data = this.data.courseData
+    wx.getStorage({
+      key: 'token',
+      success: function (res) {
+        console.log(res)
+          wx.request({
+            method: 'post',
+            url: app.globalData.baseUrl + '/v1/sub',
+            header: {
+              'Authorization': 'bearer ' + res.data,
+            },
+            data: {
+              "courseID": data.id,
+            },
+            success: function (res) {
+              wx.showToast({
+                title: '收藏成功！',
+              })
+
+            },
+            fail: function (res) {
+              wx.showToast({
+                title: res.msg,
+              })
+            }
+          })
+      },
+      fail: function (res) {
+        wx.showModal({
+          title: '提示',
+          content: '您还未登录，是否跳转至登录',
+          success: function (res) {
+            if (res.confirm) {
+              wx.navigateTo({
+                url: '../login/login',
+              })
+            } else if (res.cancel) {
+              console.log('用户点击取消')
+            }
+          },
+        })
+      }
+    })
+    wx.showToast({
+      title: '收藏成功',
+      success : function (){
+        that.setData({
+          isShouCang:true
+        })
+      }
+    })
+  },
+  // 点击更多体验课
+  moreExperience() {
+    let that = this
+    this.data.offset = this.data.offset + 1
+    wx.request({
+      url: app.globalData.baseUrl +'/v1/course?limit=3&offset='+this.data.offset+'&sortby={"created_at": "desc"}&course_status=1',
+      success : function (res) {
+        let course = res.data.data.course,
+        courseList = that.data.courseList;
+        if(course.length == 0) {
+          alert("没有更多体验课")
+        }
+        course.forEach(element => {
+          courseList.push(element)
+        });
+        that.setData({
+          courseList:courseList
+        })
+      }
+    })
+  },
+  // 点击更多正式课
+  moreOfficial() {
+    let that = this
+    this.data.offset1 = this.data.offset1 + 1
+    wx.request({
+      url: app.globalData.baseUrl +'/v1/course?limit=3&offset='+this.data.offset1+'&sortby={"created_at": "desc"}&course_status=2',
+      success : function (res) {
+        let course = res.data.data.course,
+        courseList = that.data.courseList1;
+        if(course.length == 0) {
+          alert("没有更多体验课")
+        }
+        course.forEach(element => {
+          courseList.push(element)
+        });
+        that.setData({
+          courseList1:courseList
+        })
+      }
+    })
+  }
 })
