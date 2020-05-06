48ccb3701efd83dadacb9901f78c1ddfdc71f265
huaguohao <810913476@qq.com>
2020-05-06 23:06:52 +0800

    bug

--- BaiJiaKeApp/pages/courseClass/courseClass.js
+++ BaiJiaKeApp/pages/courseClass/courseClass.js
@@ -121,6 +121,10 @@ Page({
               },
               success: function (data){
                 let hotList = data.data.data.course
+                that.setData({
+                  hotList: []
+                })
+                if(!hotList) return
                 for (let i = 0; i < hotList.length; i++) {
                   hotList[i].jvLi = that.distance(res.latitude, res.longitude, hotList[i].shopinfo.latitude, hotList[i].shopinfo.longitude)
                   hotList[i].tagList = hotList[i].shopinfo.tags !== '' ? hotList[i].shopinfo.tags.split(',') : null
@@ -320,6 +324,10 @@ Page({
       },
       success: function (res) {
         let hotList = res.data.data.course
+        that.setData({
+          hotList: []
+        })
+        if(!hotList) return
         wx.getLocation({
           success: function(res) {
             for (let i = 0; i < hotList.length; i++) {
